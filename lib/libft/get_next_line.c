f313dd381ac58f63f036bf895e8509f54072be4d
fjruiz82 <104569073+fjruiz82@users.noreply.github.com>
2023-05-29 21:11:17 +0200

    Add files via upload

--- lib/libft/get_next_line.c
+++ lib/libft/get_next_line.c
@@ -13,96 +13,81 @@
 #include "libft.h"
 #define BUFFER_SIZE 512
 
-char	*reserve_line(char *reserved)
+char	*get_until(char **buffer, int until)
 {
 	char	*line;
-	int		i;
+	char	*n_ptr;
+	char	*aux_buffer;
 
-	i = 0;
-	if (!reserved[i])
+	n_ptr = ft_strchr(*buffer, until);
+	if (!n_ptr)
 		return (0);
-	while (reserved[i] && reserved[i] != '\n')
-		i++;
-	line = (char *)malloc(i + 2);
+	line = ft_substr(*buffer, 0, n_ptr - *buffer + 1);
 	if (!line)
 		return (0);
-	i = 0;
-	while (reserved[i] && reserved[i] != '\n')
-	{
-		line[i] = reserved[i];
-		i++;
-	}
-	if (reserved[i] == '\n')
-	{
-		line[i] = reserved[i];
-		i++;
-	}
-	line[i] = '\0';
+	aux_buffer = ft_substr(*buffer, n_ptr - *buffer + 1,
+			ft_strlen_gnl(*buffer) - (n_ptr - *buffer));
+	free(*buffer);
+	if (!aux_buffer)
+		return (0);
+	*buffer = aux_buffer;
 	return (line);
 }
 
-char	*save_rest(char *reserved)
+char	*read_and_find(int fd, char **buffer,
+		char *buffer_read, ssize_t *bytes_read)
 {
-	int		i;
-	int		j;
-	char	*saved;
+	char	*line;
+	char	*buffer_aux;
 
-	i = 0;
-	while (reserved[i] && reserved[i] != '\n')
-		i++;
-	if (!reserved[i])
-	{
-		free(reserved);
+	*bytes_read = read(fd, buffer_read, BUFFER_SIZE);
+	if (*bytes_read == -1)
 		return (0);
-	}
-	saved = (char *)malloc(ft_strlen(reserved) - i + 1);
-	if (!saved)
+	buffer_aux = ft_strjoin_gnl(*buffer, buffer_read);
+	if (!buffer_aux)
 		return (0);
-	i++;
-	j = 0;
-	while (reserved[i])
-		saved[j++] = reserved[i++];
-	saved[j] = '\0';
-	free(reserved);
-	return (saved);
+	free(*buffer);
+	*buffer = buffer_aux;
+	ft_bzero(buffer_read, BUFFER_SIZE);
+	line = get_until(buffer, '\n');
+	return (line);
 }
 
-char	*reading_fd(int fd, char *reserved)
+char	*read_line(int fd, char **buffer)
 {
-	ssize_t	char_read;
-	char	*buf;
+	char	*buffer_read;
+	char	*line;
+	ssize_t	bytes_read;
 
-	buf = malloc(BUFFER_SIZE + 1);
-	if (!buf)
+	bytes_read = BUFFER_SIZE;
+	buffer_read = ft_calloc(BUFFER_SIZE + 1, sizeof(char));
+	line = 0;
+	if (!buffer_read)
 		return (0);
-	char_read = 1;
-	while (!ft_strchr(reserved, '\n') && char_read != 0)
+	while (bytes_read == BUFFER_SIZE && !line)
+		line = read_and_find(fd, buffer, buffer_read, &bytes_read);
+	free(buffer_read);
+	if (*buffer && !**buffer)
 	{
-		char_read = read(fd, buf, BUFFER_SIZE);
-		if (char_read == -1)
-		{
-			free(reserved);
-			free(buf);
-			return (0);
-		}
-		buf[char_read] = '\0';
-		reserved = ft_strjoin(reserved, buf);
+		free(*buffer);
+		*buffer = 0;
 	}
-	free(buf);
-	return (reserved);
+	if (!line && *buffer && **buffer)
+	{
+		line = ft_substr_gnl(*buffer, 0, ft_strlen(*buffer));
+		free(*buffer);
+		*buffer = 0;
+	}
+	return (line);
 }
 
 char	*get_next_line(int fd)
 {
-	static char	*reserved[1024];
+	static char	*buffers[512];
 	char		*line;
 
-	if (fd < 0 || BUFFER_SIZE <= 0 || fd > 1024)
+	if (fd < 0)
 		return (0);
-	reserved[fd] = reading_fd(fd, reserved[fd]);
-	if (!reserved[fd])
-		return (0);
-	line = reserve_line(reserved[fd]);
-	reserved[fd] = save_rest(reserved[fd]);
+	line = read_line(fd, buffers + fd);
 	return (line);
 }
---
+++ lib/libft/get_next_line_utils.c
@@ -0,0 +1,80 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line_bonus.c                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: fruiz-ca <fruiz-ca@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/08/30 09:51:51 by fruiz-ca          #+#    #+#             */
+/*   Updated: 2022/08/31 08:57:02 by fruiz-ca         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+size_t	ft_strlen_gnl(const char *s)
+{
+	size_t	i;
+
+	if (!s)
+		return (0);
+	i = 0;
+	while (s[i])
+		i++;
+	return (i);
+}
+
+char	*ft_substr_gnl(char const *s, unsigned int start, size_t len)
+{
+	char	*substr;
+	size_t	i;
+	size_t	s_len;
+
+	s_len = ft_strlen_gnl(s);
+	if (start > s_len)
+	{
+		substr = ft_calloc(1, sizeof(char));
+		if (!substr)
+			return (0);
+		return (substr);
+	}
+	if (s_len - start < len)
+		len = s_len;
+	substr = ft_calloc(len + 1, sizeof(char));
+	if (!substr)
+		return (0);
+	i = 0;
+	while (i < len && s[i + start])
+	{
+		substr[i] = s[i + start];
+		i++;
+	}
+	return (substr);
+}
+
+char	*ft_strjoin_gnl(char const *s1, char const *s2)
+{
+	char	*str;
+	size_t	s1_len;
+	size_t	s2_len;
+	size_t	i;
+
+	s1_len = ft_strlen_gnl(s1);
+	s2_len = ft_strlen_gnl(s2);
+	str = ft_calloc(s1_len + s2_len + 1, sizeof(char));
+	if (!str)
+		return (0);
+	i = 0;
+	while (i < s1_len)
+	{
+		str[i] = s1[i];
+		i++;
+	}
+	i = 0;
+	while (i < s2_len)
+	{
+		str[i + s1_len] = s2[i];
+		i++;
+	}
+	return (str);
+}
